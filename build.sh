ed6c9f6c2fd60332cb18f02aaaedc12ee2fa4ae3
Boris Burgos <bburgos@codeko.com>
2023-07-26 19:18:33 +0200

    build: generated version with custom peerjs repository

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 exec 2>&1
 
 rm -rf dist/ peerjs/
-git clone https://github.com/peers/peerjs peerjs
+git clone https://github.com/Codeko/peerjs.git --branch v1.4.7-react-native --single-branch peerjs
 cd peerjs
-git apply ../decoupling.diff
+# git apply ../decoupling.diff
 ../node_modules/.bin/parcel build --no-source-maps lib/exports.ts -d ../dist --out-file peerjs.min.js
 cd ../
 cat imports.js dist/peerjs.min.js > dist/react-native-peerjs.js
