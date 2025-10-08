a63beb04773735997ae96a62464036ae97aa1302
Javier Lorenzo <106740999+jlorenzo-codeko@users.noreply.github.com>
2025-10-08 08:43:59 +0200

    chore: update parcel to v2
    refs: #CAR-1177

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ rm -rf dist/ peerjs/
 git clone https://github.com/peers/peerjs --branch v1.4.7 --single-branch peerjs
 cd peerjs
 git apply ../decoupling.diff
-../node_modules/.bin/parcel build --no-source-maps lib/exports.ts -d ../dist --out-file peerjs.min.js
 cd ../
+node_modules/.bin/parcel build --no-source-maps peerjs/lib/exports.ts
 cat imports.js dist/peerjs.min.js > dist/react-native-peerjs.js
 rm dist/peerjs.min.js
 
